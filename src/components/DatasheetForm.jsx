b77cd8a79d5cf339b338374a02e2bc81b16a5845
mhera-c1 <mherafecabug10@gmail.com>
2025-04-29 12:07:34 +0800

    change a bit of profile asection

---
+++ src/components/DatasheetForm.jsx
@@ -0,0 +1,9 @@
+export default function DatasheetForm() {
+  return (
+    <div className="d-flex flex-column w-100">
+      <div className="w-100 datasheet-header fw-semibold text-muted border text-center py-2 rounded-2">
+        Personal Information
+      </div>
+    </div>
+  );
+}
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -9,156 +9,99 @@ import { useAuth } from "../context/AuthContext";
 import { useNavigate, useParams } from "react-router-dom";
 import { supabase } from "../supabase-client";
 import { useUsers } from "../hooks/useUsers";
+import DatasheetForm from "../components/DatasheetForm";
 
 function Home() {
-    const { user, signOutUser } = useAuth();
-    const [readOnly, setReadOnly] = useState(true);
+  const { user, signOutUser } = useAuth();
+  const [readOnly, setReadOnly] = useState(true);
 
-    const { id } = useParams();
-    const navigate = useNavigate();
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-    const { data: users, isLoading } = useUsers(); // all users already fetched
+  const { data: users, isLoading } = useUsers(); // all users already fetched
 
-    const userProfile = users?.find((user) => user.user_id === id);
+  const userProfile = users?.find((user) => user.user_id === id);
 
-    if (isLoading)
-        return (
-            <div className="center p-5">
-                <div className="spinner-border" role="status"></div>
-            </div>
-        );
+  if (isLoading)
     return (
-        <div
-            style={{ maxWidth: 900 }}
-            className="profile-page d-flex flex-column align-items-center justify-content-start min-h-100 p-0 p-md-4 mx-auto"
-        >
-            {/* header  */}
-            <div className="profile-header w-100">
-                <div className="d-flex justify-content-between p-3">
-                    <div
-                        onClick={() => navigate(-1)}
-                        className="p-2 cursor shadow center rounded-circle bg-light"
-                    >
-                        <IoMdArrowRoundBack size={20} />
-                    </div>
-                    <div
-                        style={{ zIndex: 10 }}
-                        onClick={signOutUser}
-                        className="btn shadow btn-danger fs-7 rounded-pill center px-4"
-                    >
-                        <FiLogOut /> logout
-                    </div>
-                </div>
-            </div>
-
-            {/* body  */}
-            <div className="w-100 position-relative d-flex flex-column align-items-center profile-body px-3">
-                <div className="profile-pic-container mb-4 rounded-circle p-2 center position-relative">
-                    <img
-                        src={
-                            userProfile.image_path
-                                ? userProfile.image_path
-                                : "/images/default-img.jpg"
-                        }
-                        className="rounded-circle"
-                        width={180}
-                        height={180}
-                        alt=""
-                    />
-                    <div className="check-badge">
-                        <BsPatchCheckFill size={42} color="rgb(160, 64, 230)" />
-                    </div>
-                </div>
-                <p className="text-secondary mb-2 text-center">
-                    {userProfile.email ?? "email not set"}
-                </p>
-                <h1 className="text-muted text-center fw-bold">
-                    {userProfile?.first_name} {userProfile?.middle_name}{" "}
-                    {userProfile?.last_name}
-                </h1>
-                {userProfile.position && (
-                    <i className="fs-4 text-muted mb-2 text-center">
-                        {userProfile.position}
-                    </i>
-                )}
-
-                <p className="mb-0 text-secondary text-center">
-                    {userProfile.address && userProfile.address}
-                </p>
-
-                <div
-                    style={{ gap: 8 }}
-                    className="d-flex align-items-center py-4"
-                >
-                    <button className="btn btn-success px-2 px-md-4">
-                        Update Infos
-                    </button>
-                    <button className="btn btn-outline-danger px-2 px-md-4">
-                        Delete Account
-                    </button>
-                    <button className="btn btn-outline-secondary">
-                        <MdOutlineContentCopy size={18} />
-                    </button>
-                </div>
-                <div className="border-bottom w-100 mt-3"></div>
-            </div>
-
-            {/* edit section */}
-            <div className="w-100 px-3 profile-edit-section py-5 position-relative">
-                {readOnly && <div className="edit-section-cover"></div>}
-                <div className="d-flex align-items-center mb-2 justify-content-between">
-                    <h5 className="text-muted mb-0">Personal informations</h5>
-                    <button
-                        onClick={() => setReadOnly(!readOnly)}
-                        className="btn btn-sm btn-primary px-4"
-                    >
-                        {readOnly ? "Edit" : "Save Changes"}
-                    </button>
-                </div>
-                <div className="row row-cols-1 row-cols-md-2 px-2">
-                    <div className="col px-1">
-                        <div className="mb-3">
-                            <label htmlFor="firstname" className="form-label">
-                                First Name
-                            </label>
-                            <input
-                                type="email"
-                                className="form-control bg-none"
-                                id="firstname"
-                                placeholder="name@example.com"
-                            />
-                        </div>
-                    </div>
-                    <div className="col px-1">
-                        <div className="mb-3">
-                            <label htmlFor="lastname" className="form-label">
-                                Last Name
-                            </label>
-                            <input
-                                type="email"
-                                className="form-control bg-none"
-                                id="lastname"
-                                placeholder="name@example.com"
-                            />
-                        </div>
-                    </div>
-                    <div className="col px-1">
-                        <div className="mb-3">
-                            <label htmlFor="lastname" className="form-label">
-                                Email
-                            </label>
-                            <input
-                                type="email"
-                                className="form-control bg-none"
-                                id="lastname"
-                                placeholder="name@example.com"
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+      <div className="center p-5">
+        <div className="spinner-border" role="status"></div>
+      </div>
     );
+  return (
+    <div
+      style={{ maxWidth: 900 }}
+      className="profile-page d-flex flex-column align-items-center justify-content-start min-h-100 p-0 p-md-4 mx-auto"
+    >
+      {/* header  */}
+      <div className="profile-header w-100">
+        <div className="d-flex justify-content-between p-3">
+          <div
+            onClick={() => navigate(-1)}
+            className="p-2 cursor shadow center rounded-circle bg-light"
+          >
+            <IoMdArrowRoundBack size={20} />
+          </div>
+          <div
+            style={{ zIndex: 10 }}
+            onClick={signOutUser}
+            className="btn shadow btn-danger fs-7 rounded-pill center px-4"
+          >
+            <FiLogOut /> logout
+          </div>
+        </div>
+      </div>
+
+      {/* body  */}
+      <div className="w-100 position-relative d-flex flex-column align-items-center profile-body px-3">
+        <div className="profile-pic-container mb-4 rounded-circle p-2 center position-relative">
+          <img
+            src={
+              userProfile.image_path
+                ? userProfile.image_path
+                : "/images/default-img.jpg"
+            }
+            className="rounded-circle"
+            width={180}
+            height={180}
+            alt=""
+          />
+          <div className="check-badge">
+            <BsPatchCheckFill size={42} color="rgb(160, 64, 230)" />
+          </div>
+        </div>
+        <p className="text-secondary mb-2 text-center">
+          {userProfile.email ?? "email not set"}
+        </p>
+        <h1 className="text-muted text-center fw-bold">
+          {userProfile?.first_name} {userProfile?.middle_name}{" "}
+          {userProfile?.last_name}
+        </h1>
+        {userProfile.position && (
+          <i className="fs-4 text-muted mb-2 text-center">
+            {userProfile.position}
+          </i>
+        )}
+
+        <p className="mb-0 text-secondary text-center">
+          {userProfile.address && userProfile.address}
+        </p>
+
+        <div style={{ gap: 8 }} className="d-flex align-items-center py-4">
+          <button className="btn btn-success px-2 px-md-4">Update Infos</button>
+          <button className="btn btn-outline-danger px-2 px-md-4">
+            Delete Account
+          </button>
+          <button className="btn btn-outline-secondary">
+            <MdOutlineContentCopy size={18} />
+          </button>
+        </div>
+        <div className="border-bottom w-100 mt-3"></div>
+      </div>
+
+      <DatasheetForm />
+    </div>
+  );
 }
 
 export default Home;
